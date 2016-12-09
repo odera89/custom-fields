2080c5ff08b07e6a152ccec50f49cffe65763328
Tedozi Manson <duyphan.developer@gmail.com>
2016-12-09 20:31:50 +0700

    Initial. Add documentation

---
+++ src/Hook/Actions/Store/Pages.php
@@ -0,0 +1,9 @@
+<?php namespace WebEd\Plugins\CustomFields\Hook\Actions\Store;
+
+class Pages extends AbstractStore
+{
+    /**
+     * @var string
+     */
+    protected $repositoryInterface = 'WebEd\Base\Pages\Repositories\Contracts\PageContract';
+}
